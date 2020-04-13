534774f1e510f96016c24c41bd26e8848b0f99ca
unknown <wuzp@beantechs.com>
2020-04-13 15:46:08 +0800

    init this repo

---
+++ app/src/main/java/com/example/hellobean/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.hellobean;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView mHelloTv;
+    private Button mHelloBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initView();
+        initData();
+    }
+
+    private void initView() {
+        mHelloTv = findViewById(R.id.tv_hello);
+        mHelloBtn = findViewById(R.id.btn_hello);
+    }
+
+    private void initData() {
+        mHelloTv.setText("hello Bean!");
+        mHelloBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_hello:
+                testHelloBtn();
+                break;
+        }
+    }
+
+    private void testHelloBtn() {
+        Toast.makeText(this, "Hello Bean,I am here", Toast.LENGTH_SHORT).show();
+    }
+
+}
